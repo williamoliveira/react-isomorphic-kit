99f5d756c59d932b49a17219b822abac2779f744
William Oliveira <william.oliveir4@gmail.com>
2018-04-01 09:57:19 -0300

    fix requireFromUserland

--- src/internal/utils/requireFromUserland.js
+++ src/internal/utils/requireFromUserland.js
@@ -1,7 +1,7 @@
 import { resolve as pathResolve } from 'path'
 import appRootDir from 'app-root-dir'
 
-export default relative => pathResolve(appRootDir.get(), relative)
+export default relative => require(pathResolve(appRootDir.get(), relative))
 
 if (process.env.BUILD_FLAG_IS_CLIENT === 'true') {
   throw new Error("This file shouldn't be imported on client side code")

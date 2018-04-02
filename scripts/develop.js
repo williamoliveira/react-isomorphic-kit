9c56d7799f9769ca65f30b97b7ad3372e8f556f5
William Oliveira <william.oliveir4@gmail.com>
2018-04-01 23:48:25 -0300

    added scripts

--- scripts/develop.js
+++ scripts/develop.js
@@ -1,10 +1,12 @@
+#!/usr/bin/env node
+
 const { execSync } = require('child_process')
 const path = require('path')
 
+const crossEnvPath = path.resolve(__dirname, '../node_modules/.bin/cross-env')
 const babelNodePath = path.resolve(__dirname, '../node_modules/.bin/babel-node')
-const devPath = path.resolve(__dirname, '../src/internal/development')
-const libPath = path.resolve(__dirname, '..')
+const devPath = path.resolve(__dirname, '../lib/internal/development')
 
-execSync(`cross-env DEPLOYMENT=development ${babelNodePath} ${devPath}`, {
+execSync(`${crossEnvPath} DEPLOYMENT=development ${babelNodePath} ${devPath}`, {
   stdio: 'inherit',
 })

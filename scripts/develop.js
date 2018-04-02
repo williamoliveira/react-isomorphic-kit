a63a4d7f293f2e724ed253a47123562f7fd5f057
William Oliveira <william.oliveir4@gmail.com>
2018-04-02 02:36:17 -0300

    rearanged deps

--- scripts/develop.js
+++ scripts/develop.js
@@ -1,12 +1,4 @@
 #!/usr/bin/env node
 
-const { execSync } = require('child_process')
-const path = require('path')
-
-const crossEnvPath = path.resolve(__dirname, '../node_modules/.bin/cross-env')
-const babelNodePath = path.resolve(__dirname, '../node_modules/.bin/babel-node')
-const devPath = path.resolve(__dirname, '../lib/internal/development')
-
-execSync(`${crossEnvPath} DEPLOYMENT=development ${babelNodePath} ${devPath}`, {
-  stdio: 'inherit',
-})
+require('@babel/register')
+require('../lib/internal/development')
